2e6fd76b8a6b47272a77655856c04e0857d07905
Chouteau Perry <perry.chouteau@epitech.eu>
2025-06-13 06:07:45 +0200

    init(igraphicmodule)

---
+++ includes/IGraphicModule.hpp
@@ -0,0 +1,72 @@
+/**
+ * @file IGraphicModule.hpp
+ * @author Perry Chouteau (perry.chouteau@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 11-06-2025
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#ifndef IGRAPHIC_MODULE_HPP
+#define IGRAPHIC_MODULE_HPP
+
+#include "window/ICamera.hpp"
+#include "window/IWindow.hpp"
+
+#include "graphic/IModel.hpp"
+#include "graphic/IPolygon.hpp"
+#include "graphic/ISprite.hpp"
+#include "graphic/IText.hpp"
+
+#include "event/IEvent.hpp"
+#include "event/IKeyboard.hpp"
+#include "event/IMouse.hpp"
+
+#include "audio/IMusic.hpp"
+#include "audio/ISound.hpp"
+
+class IGraphicModule: public IModule {
+    virtual ~IGraphicModule();
+
+    //camera
+    virtual graphic::ICamera *createCamera() = 0;
+    virtual void deleteCamera(graphic::ICamera *) = 0;
+    //window
+    virtual graphic::IWindow *createWindow(__int32_t screenWidth, __int32_t screenHeight, std::string title) = 0;
+    virtual void deleteWindow(graphic::IWindow *) = 0;
+
+    //model
+    virtual graphic::IModel *createModel() = 0;
+    virtual void deleteModel(graphic::IModel *model) = 0;
+
+    //polygon
+    virtual graphic::IPolygon *createPolygon(std::vector<__v2f_t> points) = 0;
+    virtual void deletePolygon(graphic::IPolygon *polygon) = 0;
+    //sprite
+    virtual graphic::ISprite *createSprite(std::string sprite) = 0;
+    virtual void deleteSprite(graphic::ISprite *sprite) = 0;
+    //text
+    virtual graphic::IText *createText(std::string text, std::string font) = 0;
+    virtual void deleteText(graphic::IText *text) = 0;
+
+    //event
+    virtual graphic::IEvent *createEvent() = 0;
+    virtual void deleteEvent(graphic::IEvent *event) = 0;
+    //keyboard
+    virtual graphic::IKeyboard *createKeyboard(graphic::IEvent *event) = 0;
+    virtual void deleteKeyboard(graphic::IKeyboard *keyboard) = 0;
+    //mouse
+    virtual graphic::IMouse *createMouse(graphic::IEvent *event) = 0;
+    virtual void deleteMouse(graphic::IMouse *mouse) = 0;
+
+    //music
+    virtual graphic::IMusic *createMusic(std::string path) = 0;
+    virtual void deleteMusic(graphic::IMusic *music) = 0;
+    //sound
+    virtual graphic::ISound *createSound(std::string path) = 0;
+    virtual void deleteSound(graphic::ISound *sound) = 0;
+};
+
+#endif /* !IGRAPHIC_MODULE_HPP */
